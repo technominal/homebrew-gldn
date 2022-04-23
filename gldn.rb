f388ca832603eaac19c106b4e33e7b9f004c60e1
Dean Hallman <dean@foresight.dev>
2022-04-23 07:04:12 -0400

    updating sha256

--- gldn.rb
+++ gldn.rb
@@ -1,8 +1,8 @@
 class Gldn < Formula
   desc 'Golden: A tool for managing Dotfiles and setup scripts'
   homepage 'https://github.com/technominal/gldn'
-  url 'https://github.com/technominal/gldn/archive/refs/tags/v0.1.1.tar.gz'
-  sha256 'fde32164162588b8a4fa310eef6a5c0cb60d3e6c03a671706c41a4ca56e5adb0'
+  url 'https://rubygems.org/downloads/gldn-0.1.1.gem'
+  sha256 'c092d22be9077a4679c4313f53c8ce3347acbf29cf49fec52329a4ac41dd998a'
   license 'MIT'
 
   def install
