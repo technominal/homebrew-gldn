0f267ca2f55b4633158605217bb0ccb81b701f02
Dean Hallman <dean@foresight.dev>
2022-04-23 06:47:12 -0400

    updating sha256

--- gldn.rb
+++ gldn.rb
@@ -1,8 +1,8 @@
 class Gldn < Formula
   desc 'Golden: A tool for managing Dotfiles and setup scripts'
   homepage 'https://github.com/technominal/gldn'
-  url 'https://github.com/technominal/gldn/archive/refs/tags/v0.1.0.tar.gz'
-  sha256 '7bb6d2dfc906be5df2a3657f75a4ab538b6f14456b479a4689ad90d5b83265c4'
+  url 'https://github.com/technominal/gldn/archive/refs/tags/v0.1.1.tar.gz'
+  sha256 'fde32164162588b8a4fa310eef6a5c0cb60d3e6c03a671706c41a4ca56e5adb0'
   license 'MIT'
 
   def install
