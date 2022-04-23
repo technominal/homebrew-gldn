4020fed14e6e69e4c5740d5cb57338fdd6fefd2a
Dean Hallman <dean@foresight.dev>
2022-04-23 06:28:27 -0400

    updating version

--- gldn.rb
+++ gldn.rb
@@ -1,8 +1,9 @@
 class Gldn < Formula
   desc 'Golden: A tool for managing Dotfiles and setup scripts'
   homepage 'https://github.com/technominal/gldn'
-  url 'https://github.com/technominal/gldn/archive/refs/tags/v0.1.0.tar.gz'
+  url 'https://github.com/technominal/gldn/archive/refs/tags/v0.1.1.tar.gz'
   sha256 '7bb6d2dfc906be5df2a3657f75a4ab538b6f14456b479a4689ad90d5b83265c4'
+   14a1d1d
   license 'MIT'
 
   def install
